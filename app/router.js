3b7d7d9cdfdaa128d451fc64e394397c632b2be9
jaejunmin <jaejunmi@usc.edu>
2018-09-21 15:57:11 -0700

    lab1

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('about', function() {
+    this.route('info', {path: ':info'});
+  });
 });
 
 export default Router;
